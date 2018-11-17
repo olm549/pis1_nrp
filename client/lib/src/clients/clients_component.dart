e4f7f7778fd6aa9032e82ba28d0fde622862a27d
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2018-11-17 20:35:14 +0100

    Funcionalidad crear cliente

--- client/lib/src/clients/clients_component.dart
+++ client/lib/src/clients/clients_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 import '../model/client.dart';
 import 'mock_clients.dart';
@@ -15,24 +16,89 @@ import './clients_service.dart';
   templateUrl: 'clients_component.html',
   directives: [
     coreDirectives,
+    formDirectives,
     MaterialPersistentDrawerDirective,
     MaterialListComponent,
     MaterialListItemComponent,
     MaterialButtonComponent,
+    MaterialInputComponent,
+    materialInputDirectives,
   ],
   providers: [const ClassProvider(ClientsService)],
 )
 class ClientsComponent {
   final ClientsService clientsService;
 
+  @Input() bool isEditing = true;
+
   Client selected;
   List<Client> clients = mockClients;
+  bool createClientPanel = false;
+  String clientIdToAdd;
+  String nameToAdd;
+  String surnameToAdd;
+  bool showIdNull, showNameNull, showSurnameNull = false;
+  
 
   ClientsComponent(this.clientsService);
 
-  void onSelect(Client client) => selected = client;
+  void onSelect(Client client){
+    createClientPanel = false;
+    resetPanel();
+    selected = client;
+  }
 
   void createClient() async {
-    await clientsService.createClient();
+
+    if(clientIdToAdd==null || clientIdToAdd == "" ){
+      showIdNull = true;
+    }else{
+      showIdNull = false;
+    }
+    
+    if(nameToAdd == null || nameToAdd == "" ){
+      showNameNull = true;
+    }else{
+      showNameNull = false;
+    }
+    
+    if(surnameToAdd == null || surnameToAdd == "" ){
+      showSurnameNull = true;
+    }else{
+      showSurnameNull = false;
+    }
+
+    if(clientIdToAdd==null || nameToAdd == null || surnameToAdd == null) return;
+
+    print("clientIdToAdd = "+clientIdToAdd);
+    print("name = "+nameToAdd);
+    print("surname = "+surnameToAdd);
+
+    Client createCl = await clientsService.createClient(clientIdToAdd,nameToAdd,surnameToAdd);
+    if(createCl != null) createClientPanel = false;
+    
+    
+  }
+
+  void editClient() async{
+
+  }
+
+  void newClient(){
+    isEditing = false;
+    if(createClientPanel == true) resetPanel();
+    else createClientPanel = true;
+    if(selected!=null) selected = null;
+    
+  }
+
+  void resetPanel(){
+    createClientPanel = false;
+    clientIdToAdd = "";
+    nameToAdd = "";
+    surnameToAdd = "";
+    showIdNull = false;
+    showNameNull = false;
+    showSurnameNull = false;
   }
 }
