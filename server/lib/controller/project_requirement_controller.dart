4f49cfac70d189b93fc593d02a29a7296bb99813
Víctor Vázquez <victorvazrod@gmail.com>
2018-11-19 12:34:01 +0100

    Añadida documentación de controladores de tablas intermedias

--- server/lib/controller/project_requirement_controller.dart
+++ server/lib/controller/project_requirement_controller.dart
@@ -3,11 +3,16 @@ import '../model/requirement_value.dart';
 
 import '../nrp_server.dart';
 
+/// A controller that handles requests for the [ProjectRequirement] resources.
 class ProjectRequirementController extends ResourceController {
   ProjectRequirementController(this.context);
 
   ManagedContext context;
 
+  /// Fetches all requirement's data for a project.
+  ///
+  /// A list of [ProjectRequirement] resources joined with the requirement's
+  /// data is returned in the body of a 200 response.
   @Operation.get('projectID')
   Future<Response> getAllProjectRequirements(
     @Bind.path('projectID') int projectID,
@@ -24,6 +29,14 @@ class ProjectRequirementController extends ResourceController {
     return Response.ok(fetchedProjectRequirements);
   }
 
+  /// Fetches a certain requirement's data for a project.
+  ///
+  /// The [ProjectRequirement.id] isn't used to fetch the resource. Instead,
+  /// both [ProjectRequirement.project.id] and [ProjectRequirement.requirement.id] are
+  /// taken from the request's path.
+  ///
+  /// Returns a 200 response with the [ProjectRequirement] resource and the
+  /// requirement's data.
   @Operation.get('projectID', 'requirementID')
   Future<Response> getProjectRequirement(
     @Bind.path('projectID') int projectID,
@@ -40,6 +53,13 @@ class ProjectRequirementController extends ResourceController {
     return Response.ok(fetchedProjectRequirement);
   }
 
+  /// Inserts a new project requirement.
+  ///
+  /// Values of the new resource, including [ProjectRequirement.requirement.id],
+  /// must be sent in the request's body and are parsed into a
+  /// [ProjectRequirement] object for insertion.
+  ///
+  /// Returns a 200 response with the new resource.
   @Operation.post('projectID')
   Future<Response> addProjectRequirement(
     @Bind.path('projectID') int projectID,
@@ -55,6 +75,16 @@ class ProjectRequirementController extends ResourceController {
     return Response.ok(insertedProjectRequirement);
   }
 
+  /// Updates a requirement's data for a project
+  ///
+  /// The [ProjectRequirement.id] isn't used to identify the resource. Instead,
+  /// both [ProjectRequirement.project.id] and [ProjectRequirement.requirement.id] are
+  /// taken from the request's path.
+  ///
+  /// New values for the resource must be sent in the request's body and
+  /// are parsed into a [ProjectRequirement] object.
+  ///
+  /// Returns a 200 response with the updated resource.
   @Operation.put('projectID', 'requirementID')
   Future<Response> modifyProjectRequirement(
     @Bind.path('projectID') int projectID,
@@ -72,6 +102,16 @@ class ProjectRequirementController extends ResourceController {
     return Response.ok(updatedProjectRequirement);
   }
 
+  /// Deletes a requirement's data for a project.
+  ///
+  /// The [ProjectRequirement.id] isn't used to identify the resource. Instead,
+  /// both [ProjectRequirement.project.id] and [ProjectRequirement.requirement.id] are
+  /// taken from the request's path.
+  ///
+  /// Also deletes any resource of [RequirementValue] that existed for the
+  /// requirement in the project.
+  ///
+  /// Returns a 200 response with the number of rows deleted.
   @Operation.delete('projectID', 'requirementID')
   Future<Response> deleteProjectRequirement(
     @Bind.path('projectID') int projectID,
--- server/lib/controller/requirement_value_controller.dart
+++ server/lib/controller/requirement_value_controller.dart
@@ -2,11 +2,16 @@ import '../model/requirement_value.dart';
 
 import '../nrp_server.dart';
 
+/// A controller that handles requests for the [RequirementValue] resources.
 class RequirementValueController extends ResourceController {
   RequirementValueController(this.context);
 
   ManagedContext context;
 
+  /// Fetches all requirement values for a project.
+  ///
+  /// A list of [RequirementValue] resources joined with the client's data
+  /// is returned in the body of a 200 response.
   @Operation.get('projectID', 'requirementID')
   Future<Response> getRequirementValues(
     @Bind.path('projectID') int projectID,
@@ -24,6 +29,14 @@ class RequirementValueController extends ResourceController {
     return Response.ok(fetchedRequirementValues);
   }
 
+  /// Fetches a requirement value for a client in a project.
+  ///
+  /// The [RequirementValue.id] isn't used to identify the resource. Instead,
+  /// [RequirementValue.project.id], [RequirementValue.requirement.id] and
+  /// [RequirementValue.client.id] are taken from the request´s path.
+  ///
+  /// Returns a 200 response with the [RequirementValue] resource and the
+  /// client's data.
   @Operation.get('projectID', 'requirementID', 'clientID')
   Future<Response> getRequirementValue(
     @Bind.path('projectID') int projectID,
@@ -41,6 +54,13 @@ class RequirementValueController extends ResourceController {
     return Response.ok(fetchedRequirementValue);
   }
 
+  /// Inserts a new requirement value for a project.
+  ///
+  /// Values for the new resource, including [RequirementValue.client.id],
+  /// must be sent in the request's body and are parsed into a [RequirementValue]
+  /// object for insertion.
+  ///
+  /// Returns a 200 response with the new resource.
   @Operation.post('projectID', 'requirementID')
   Future<Response> addRequirementValue(
     @Bind.path('projectID') int projectID,
@@ -57,6 +77,16 @@ class RequirementValueController extends ResourceController {
     return Response.ok(insertedRequirementValue);
   }
 
+  /// Updates a requirement value for a client in a project.
+  ///
+  /// The [RequirementValue.id] isn't used to identify the resource. Instead,
+  /// [RequirementValue.project.id], [RequirementValue.requirement.id] and
+  /// [RequirementValue.client.id] are taken from the request´s path.
+  ///
+  /// New values for the resource must be sent in the request's body and
+  /// are parsed into a [RequirementValue] object.
+  ///
+  /// Returns a 200 response with the updated resource.
   @Operation.put('projectID', 'requirementID', 'clientID')
   Future<Response> modifyRequirementValues(
     @Bind.path('projectID') int projectID,
@@ -76,6 +106,13 @@ class RequirementValueController extends ResourceController {
     return Response.ok(updatedRequirementValue);
   }
 
+  /// Deletes a requirement value for a client in a project.
+  ///
+  /// The [RequirementValue.id] isn't used to identify the resource. Instead,
+  /// [RequirementValue.project.id], [RequirementValue.requirement.id] and
+  /// [RequirementValue.client.id] are taken from the request´s path.
+  ///
+  /// Returns a 200 response with the number of rows deleted.
   @Operation.delete('projectID', 'requirementID', 'clientID')
   Future<Response> deleteRequirementValue(
     @Bind.path('projectID') int projectID,
