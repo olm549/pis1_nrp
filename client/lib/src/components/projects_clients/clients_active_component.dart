4df1ec2a8c286d0a0b103487813486278747830d
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2018-11-26 01:43:24 +0100

    Añadida ruta requisitos activos

--- client/lib/src/components/projects_clients/clients_active_component.dart
+++ client/lib/src/components/projects_clients/clients_active_component.dart
@@ -53,102 +53,12 @@ class ClientsComponent implements OnInit {
 
   void onSelect(Client client) {
     createClientPanel = false;
-    resetPanel();
     selected = client;
   }
 
-  // Método para confirmar creación del cliente
-  void createClient() async {
-    if (!comprobarClient()) return;
+  //Añadir cliente a un proyecto
+  void addToProject(){
 
-    Client createdClient = await clientsService.createClient(
-        clientIdToAdd, nameToAdd, surnameToAdd);
-
-    if (createdClient != null) {
-      clients.add(createdClient);
-
-      createClientPanel = false;
-    }
   }
-
-  // Método para abrir la ventana de edición de un cliente
-  void editClient() async {
-    isEditing = true;
-    createClientPanel = true;
-
-    clientIdToAdd = selected.clientID;
-    nameToAdd = selected.name;
-    surnameToAdd = selected.surname;
-
-    selected = null;
-  }
-
-  // Método para confirmar la edición de un cliente
-  void confirmEditClient() async {
-    if (!comprobarClient()) return;
-
-    Client updatedClient = await clientsService.updateClient(
-      selected.id,
-      selected.clientID,
-      selected.name,
-      selected.surname,
-    );
-
-    // TODO: Revisar implementación.
-    clients.remove(selected);
-
-    isEditing = false;
-    createClientPanel = false;
-
-    clients.add(updatedClient);
-  }
-
-  // Método para eliminar un cliente
-  void deleteClient() async {
-    bool deleted = await clientsService.deleteClient(selected.id);
-
-    if (deleted) {
-      clients.remove(selected);
-
-      selected = null;
-    }
-  }
-
-  // Método para abrir el panel de introducir formulario para agregar un cliente
-  void newClient() {
-    if (createClientPanel == true)
-      resetPanel();
-    else
-      createClientPanel = true;
-
-    isEditing = false;
-
-    if (selected != null) selected = null;
-  }
-
-  // Resetear valores del panel
-  void resetPanel() {
-    if (isEditing == false) createClientPanel = false;
-
-    clientIdToAdd = "";
-    nameToAdd = "";
-    surnameToAdd = "";
-  }
-
-  // Método para cerrar la vista de editar cliente
-  void cancelEditClient() {
-    resetPanel();
-    isEditing = false;
-    createClientPanel = false;
-  }
-
-  // Método para comprobar los valores del formulario
-  bool comprobarClient() {
-    if (clientIdToAdd == null || nameToAdd == null || surnameToAdd == null)
-      return false;
-    if (clientIdToAdd == "" || nameToAdd == "" || surnameToAdd == "")
-      return false;
-
-    return true;
-  }
+  
 }
--- client/lib/src/utils/routing.dart
+++ client/lib/src/utils/routing.dart
@@ -10,6 +10,8 @@ import '../components/requirements/requirements_component.template.dart'
     as requirements_template;
 import '../components/projects_clients/clients_active_component.template.dart'
     as clients_active_template;
+import '../components/projects_requirements/requirements_active_component.template.dart' 
+    as requirements_active_template;
 
 class Paths {
   static final auth = RoutePath(path: 'auth');
@@ -17,6 +19,7 @@ class Paths {
   static final clients = RoutePath(path: 'clients');
   static final requirements = RoutePath(path: 'requirements');
   static final clients_active = RoutePath(path: 'clients_active');
+  static final requirements_active = RoutePath(path: 'requirements_active');
 }
 
 class Routes {
@@ -46,11 +49,17 @@ class Routes {
     component: clients_active_template.ClientsComponentNgFactory,
   );
 
+  static final requirements_active = RouteDefinition(
+    routePath: Paths.requirements_active,
+    component: requirements_active_template.RequirementsComponentNgFactory,
+  );
+
   static final all = <RouteDefinition>[
     auth,
     projects,
     clients,
     requirements,
     clients_active,
+    requirements_active,
   ];
 }
