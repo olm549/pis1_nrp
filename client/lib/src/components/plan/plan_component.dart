a78eaa9826fb8812812996397a076a03b83c7ae8
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2018-12-11 19:18:48 +0100

    Mensajes error al planificar

--- client/lib/src/components/plan/plan_component.dart
+++ client/lib/src/components/plan/plan_component.dart
@@ -1,12 +1,15 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:client/src/services/project_requirements/project_requirements_service.dart';
 import 'package:client/src/utils/routing.dart';
-import '../../models/requirement.dart';
+import '../../models/project.dart';
 import '../../models/project_requirement.dart';
 import '../../services/plan/plan_service.dart';
 import '../../services/plan/http_plan.dart';
+import '../../services/user/user_service.dart';
+import '../../services/project_requirements/project_requirements_service.dart';
+import '../../services/clients/clients_service.dart';
+import '../../models/client.dart';
 
 import '../../utils/routing.dart';
 
@@ -38,17 +41,28 @@ import '../../utils/routing.dart';
 )
 class PlanComponent implements OnInit {
   final PlanService planService;
+  final UserService userService;
+  //final ClientsService clientsService;
+  //final ProjectRequirementService projectRequirementService;
 
   ProjectRequirement selected;
   List<ProjectRequirement> requirements;
+  bool someError = false;
   bool errorEstimatedEffort = false;
+  bool errorLimitEffort = false;
+  Project project;
+  String effortLimitProject = "0";
+  //List<Client> clients;
 
-  PlanComponent(this.planService);
+  PlanComponent(this.planService, this.userService,);
 
   @override
   void ngOnInit() async {
     requirements = await planService.getProjectRequirements();
-    comprobarErrorEsfuerzoEstimado();
+    project = userService.getActiveProject();
+    effortLimitProject = project.effortLimit.toString();
+    someError = comprobarErrores();
+    //clients = await clientsService.getClients();   
   }
 
   void onSelect(ProjectRequirement requirement) {
@@ -56,18 +70,33 @@ class PlanComponent implements OnInit {
   }
 
   void automaticPlanning(){
-
-    comprobarErrorEsfuerzoEstimado();
-
-    if(errorEstimatedEffort) return; 
+    if(comprobarErrores()) return;
   }
 
-  void comprobarErrorEsfuerzoEstimado(){
+  bool comprobarErrores(){
+    if(errorEsfuerzoEstimado()) return true;
+    if(errorEsfuerzoLimite()) return true;
+
+    return false;
+  }
+
+  bool errorEsfuerzoEstimado(){
     errorEstimatedEffort = false;
     for(ProjectRequirement r in requirements){
         if(r.estimatedEffort == 0){
           errorEstimatedEffort = true;
+          return true;
         }
     }
+    return false;
+  }
+
+  bool errorEsfuerzoLimite(){
+    errorLimitEffort = false;
+    if(project.effortLimit == 0) {
+      errorLimitEffort = true;
+      return true;
+    }
+    return false;
   }
 }
